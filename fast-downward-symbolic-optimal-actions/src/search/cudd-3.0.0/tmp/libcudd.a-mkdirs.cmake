0b562344e68aca26875db74577c22cc725559e26
Czubbi <piotr.gzubicki97@gmail.com>
2024-02-25 11:46:10 +0100

    added FD-symbolic

---
+++ fast-downward-symbolic-optimal-actions/src/search/cudd-3.0.0/tmp/libcudd.a-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/Users/piotrgzubicki/School/projectGNNs/data_generator/fast-downward-symbolic-optimal-actions/src/search/cudd-3.0.0"
+  "/Users/piotrgzubicki/School/projectGNNs/data_generator/fast-downward-symbolic-optimal-actions/src/search/cudd-3.0.0/src/libcudd.a-build"
+  "/Users/piotrgzubicki/School/projectGNNs/data_generator/fast-downward-symbolic-optimal-actions/src/search/cudd-3.0.0"
+  "/Users/piotrgzubicki/School/projectGNNs/data_generator/fast-downward-symbolic-optimal-actions/src/search/cudd-3.0.0/tmp"
+  "/Users/piotrgzubicki/School/projectGNNs/data_generator/fast-downward-symbolic-optimal-actions/src/search/cudd-3.0.0/src/libcudd.a-stamp"
+  "/Users/piotrgzubicki/School/projectGNNs/data_generator/fast-downward-symbolic-optimal-actions/src/search/cudd-3.0.0/src"
+  "/Users/piotrgzubicki/School/projectGNNs/data_generator/fast-downward-symbolic-optimal-actions/src/search/cudd-3.0.0/src/libcudd.a-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/piotrgzubicki/School/projectGNNs/data_generator/fast-downward-symbolic-optimal-actions/src/search/cudd-3.0.0/src/libcudd.a-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/Users/piotrgzubicki/School/projectGNNs/data_generator/fast-downward-symbolic-optimal-actions/src/search/cudd-3.0.0/src/libcudd.a-stamp${cfgdir}") # cfgdir has leading slash
+endif()
